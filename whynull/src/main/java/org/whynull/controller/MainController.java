5039f49f62cf34a7acaacc3e99f33b68dcd9781a
akanesasu <megazero56@gmail.com>
2022-10-14 07:43:43 +0900

    added main page

---
+++ whynull/src/main/java/org/whynull/controller/MainController.java
@@ -0,0 +1,14 @@
+package org.whynull.controller;
+
+import lombok.extern.log4j.Log4j2;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+@Log4j2
+public class MainController {
+    @GetMapping("/main")
+    public String viewMain() {
+        return "main";
+    }
+}
